33dc40850de99bf7b594f2efbeeee64788f9e0d8
Ousmane FALL <81798219+ousmane-fall@users.noreply.github.com>
2024-05-13 10:43:07 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,35 +3,33 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "square") == 0) { // Ajout de la condition pour la fonction au carré
-		r = _square(atof(a)); // Appel de la fonction au carré
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
+int main(int argc, char *argv[]) {
+    if (argc == 4) {
+        char* op = argv[1];
+        char* a = argv[2];
+        char* b = argv[3];
+        double r = 0;
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(b));
+            printf("%lf\n", r);
+        } else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+            printf("%lf\n", r);
+        } else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+            printf("%lf\n", r);
+        } else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+            printf("%lf\n", r);
+        } else if (strcmp(op, "square") == 0) {
+            r = _square(atof(a));
+            printf("%lf\n", r);
+        } else {
+            printf("Erreur : Opérateur non reconnu\n");
+        }
+    } else {
+        printf("Erreur : Nombre incorrect d'arguments\n");
+    }
+    return 0;
 }
+
