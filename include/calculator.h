f1f0f655039de77be1da5ce1011d7fad42451cf2
Ousmane FALL <81798219+ousmane-fall@users.noreply.github.com>
2024-05-13 10:40:12 +0200

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,4 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
 double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+
+// Ajout du prototype de fonction pour la fonction au carré
+double _square(double x);
+
+#endif /* CALCULATOR_H */
