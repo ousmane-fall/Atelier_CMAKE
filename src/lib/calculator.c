488b3510f64e6c2ca937301ba8c5c7e6806596eb
Ousmane FALL <81798219+ousmane-fall@users.noreply.github.com>
2024-05-13 10:35:59 +0200

    Update calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -1,18 +1,22 @@
 #include "./../../include/calculator.h"
 
 double _add(double a, double b) {
-	return a+b;
+    return a + b;
 }
 
 double _sub(double a, double b) {
-	return a-b;
+    return a - b;
 }
 
 double _mul(double a, double b) {
-	return a*b;
+    return a * b;
 }
 
 double _div(double a, double b) {
-	return a/b;
+    return a / b;
 }
-	
+
+// Définition de la fonction au carré
+double _square(double x) {
+    return x * x;
+}
